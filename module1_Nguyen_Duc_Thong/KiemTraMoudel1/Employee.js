31d6b8629361e2b3225879604c2a3f358ca2988f
NguyenDucThong <Nguyenducthong22>
2020-02-20 13:38:19 +0700

    ok

---
+++ module1_Nguyen_Duc_Thong/KiemTraMoudel1/Employee.js
@@ -0,0 +1,64 @@
+let Employee=function () {
+    this.getName=function () {
+        return this.name;
+    };
+    this.setName=function (name) {
+        this.name=name;
+    };
+
+    this.getNameSV=function () {
+        return this.nameSV;
+    };
+    this.setNameSV=function (nameSV) {
+        this.nameSV=nameSV;
+    };
+
+    this.getMatKhau=function () {
+        return this.matKhau;
+    };
+    this.setMatKhau=function (matKhau) {
+        this.matKhau=matKhau;
+    };
+
+    this.getNgaySinh=function () {
+        return this.ngaySinh;
+    };
+    this.setNgaySinh=function (ngaySinh) {
+        return this.ngaySinh=ngaySinh;
+    };
+
+    this.getCMND=function () {
+        return this.soCMND;
+    };
+    this.setName=function (soCMND) {
+        return this.soCMND=soCMND;
+    };
+
+    this.getSex=function () {
+        return this.sex;
+    };
+    this.setSex=function (sex) {
+        this.name=sex;
+    };
+
+    this.getDiaChi=function () {
+        return this.diaChi;
+    };
+    this.setDiaChi=function (diaChi) {
+        this.diaChi=diaChi;
+    };
+
+    this.getSdt=function () {
+        return this.Sdt;
+    };
+    this.setSdt=function (sdt) {
+        this.sdt=sdt;
+    };
+
+    this.getName=function () {
+        return this.email;
+    };
+    this.setEmail=function (email) {
+        this.emaik=email;
+    };
+};
