05b819a0af6dfe854e5a872e9c52643b015468e6
NguyenDucThong <Nguyenducthong22>
2020-02-21 16:53:51 +0700

    okk

--- Module2_Nguyen_Duc_Thong/src/_1_NgonNguJavaCoBan/ThucHanh/Debug.java
+++ Module2_Nguyen_Duc_Thong/src/_1_NgonNguJavaCoBan/ThucHanh/Debug.java
@@ -1,31 +1,29 @@
 package _1_NgonNguJavaCoBan.ThucHanh;
-import javax.swing.*;
 import java.util.Scanner;
-public class PhuongTrinhBacNhat {
+public class Debug {
     public static void main(String[] args) {
+        System.out.println("Giai phuong trinh bac nhat dang (ax+b=0)");
         double a,b,ketQua;
-
-        System.out.println("Giai phuong trinh bac nhat (dang ax+b=0) ");
-        System.out.println("Vui long nhap so ");
         Scanner scanner=new Scanner(System.in);
         System.out.println("Nhap a : ");
         a= scanner.nextDouble();
         System.out.println("Nhap b : ");
         b= scanner.nextDouble();
-        if (a!=0) {
-            if (b == 0) {
+        if (a!=0){
+            if (b==0){
                 System.out.print("x = 0");
-            } else {
-                ketQua = (-b) / a;
-                System.out.print("x = " + ketQua);
+            }
+            else {
+                ketQua=(-b)/a;
+                System.out.print("x = "+ketQua);
             }
         }
         else {
             if (b==0){
-                System.out.print("Phuong trinh co vo so nghiem");
+                System.out.print("Phuong trinh co vo so nghiem ");
             }
-            else{
-                System.out.print("Phuong trinh  vo nghiem");
+            else {
+                System.out.print("Phuong trinh khong co nghiem ");
             }
         }
     }
