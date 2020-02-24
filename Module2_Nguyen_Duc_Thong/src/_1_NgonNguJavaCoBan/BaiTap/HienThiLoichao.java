cf3d53067f4a60ed595f3fdf8f9c90f10ee63409
NguyenDucThong <Nguyenducthong22>
2020-02-24 09:15:57 +0700

    fas

---
+++ Module2_Nguyen_Duc_Thong/src/_1_NgonNguJavaCoBan/BaiTap/HienThiLoichao.java
@@ -0,0 +1,17 @@
+package _1_NgonNguJavaCoBan.BaiTap;
+import java.util.Scanner;
+public class HienThiLoichao {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter your fullname : ");
+        String name =scanner.nextLine();
+        name=name.trim();
+        name=name.toLowerCase();
+        String nameReplace;
+        for (int i=0;i<name.length();i++){
+            if (name.charAt(i).e" "&&name.charAt(i-1)){}
+        }
+
+        System.out.print("Hello "+nameReplace+" !!!");
+    }
+}
