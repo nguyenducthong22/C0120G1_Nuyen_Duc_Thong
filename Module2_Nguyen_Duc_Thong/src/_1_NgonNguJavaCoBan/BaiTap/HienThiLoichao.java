edf23a12eef0dc8f906887bc7442ab06e37d8b7a
NguyenDucThong <Nguyenducthong22>
2020-02-24 11:28:57 +0700

    asdas

--- Module2_Nguyen_Duc_Thong/src/_1_NgonNguJavaCoBan/BaiTap/HienThiLoichao.java
+++ Module2_Nguyen_Duc_Thong/src/_1_NgonNguJavaCoBan/BaiTap/HienThiLoichao.java
@@ -3,15 +3,8 @@ import java.util.Scanner;
 public class HienThiLoichao {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Enter your fullname : ");
+        System.out.print("Enter your name : ");
         String name =scanner.nextLine();
-        name=name.trim();
-        name=name.toLowerCase();
-        String nameReplace;
-        for (int i=0;i<name.length();i++){
-            if (name.charAt(i).e" "&&name.charAt(i-1)){}
-        }
-
-        System.out.print("Hello "+nameReplace+" !!!");
+        System.out.print("Hello "+name+" !!!");
     }
 }
