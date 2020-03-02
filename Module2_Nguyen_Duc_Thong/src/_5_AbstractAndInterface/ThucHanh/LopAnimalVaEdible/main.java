70b1e41c24fcaf8d62f3364ca69a6601f6005461
NguyenDucThong <Nguyenducthong22>
2020-03-02 14:40:38 +0700

    sda

--- Module2_Nguyen_Duc_Thong/src/_5_AbstractAndInterface/ThucHanh/LopAnimalVaEdible/main.java
+++ Module2_Nguyen_Duc_Thong/src/_5_AbstractAndInterface/ThucHanh/LopAnimalVaEdible/main.java
@@ -2,14 +2,23 @@ package _5_AbstractAndInterface.ThucHanh.LopAnimalVaEdible;
 
 public class main {
     public static void main(String[] args) {
-        Orange orange=new Orange();
-        System.out.println(orange.howToEat());
-        Apple apple=new Apple();
-        System.out.println(apple.howToEat());
-        Chicken chicken=new Chicken();
-        System.out.println(chicken.howToEat());
-        System.out.println(chicken.makeSound());
-        Tiger tiger=new Tiger();
-        System.out.println(tiger.makeSound());
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal : animals) {
+            System.out.println(animal.makeSound());
+
+            if (animal instanceof Chicken) {
+                Edible edible = (Chicken) animal;
+                System.out.println(edible.howToEat());
+            }
+        }
+
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.howToEat());
+        }
     }
 }
