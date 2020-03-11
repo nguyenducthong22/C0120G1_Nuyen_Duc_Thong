577e040c0dc717f7246352983eb5302eb6a632bf
NguyenDucThong <Nguyenducthong22>
2020-03-11 09:30:09 +0700

    :(

---
+++ Module2_Nguyen_Duc_Thong/src/CaseStudy2/Views/Run.java
@@ -0,0 +1,9 @@
+package CaseStudy2.Views;
+import CaseStudy2.Controllers.MainController;
+
+public class Run {
+    public static void main(String[] args) {
+
+        MainController.displayMainMenu();
+    }
+}
