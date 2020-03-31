6a445049c7b63b1a98431e39ffc2e99f7badca03
NguyenDucThong <Nguyenducthong22>
2020-03-31 10:41:48 +0700

    xcz

--- Module2_Nguyen_Duc_Thong_JavaWeb/src/_TongQuanVeWeb/ThucHanh/ChuyenDoiTienTe.java
+++ Module2_Nguyen_Duc_Thong_JavaWeb/src/_TongQuanVeWeb/ThucHanh/ChuyenDoiTienTe.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ConverterServlet", urlPatterns = "/convert")
+@WebServlet(name = "convert", urlPatterns = "/convert")
 public class ChuyenDoiTienTe extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         float rate = Float.parseFloat(request.getParameter("rate"));
