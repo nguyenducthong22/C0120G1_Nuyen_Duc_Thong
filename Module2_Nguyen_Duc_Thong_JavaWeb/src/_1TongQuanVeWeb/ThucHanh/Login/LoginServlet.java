13effe79685f64a2d492284d1d0d0445fc17ee55
NguyenDucThong <Nguyenducthong22>
2020-03-31 08:27:33 +0700

    sad

--- Module2_Nguyen_Duc_Thong_JavaWeb/src/_1TongQuanVeWeb/ThucHanh/Login/LoginServlet.java
+++ Module2_Nguyen_Duc_Thong_JavaWeb/src/_1TongQuanVeWeb/ThucHanh/Login/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "LoginServlet")
+@WebServlet(name = "LoginServlet", urlPatterns = "/index")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             String username = request.getParameter("username");
