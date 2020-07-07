133c20f4956fecf7bada39eb7cad4d9e0deccf8b
NguyenDucThong <Nguyenducthong22>
2020-07-07 10:12:16 +0700

    's'

--- Module2_Web(2)/demo2/src/main/java/com/codegym/demo2/service/TypeCustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.demo2.service;
-
-import com.codegym.demo2.model.TypeCustomer;
-
-@org.springframework.stereotype.Service
-
-public interface TypeCustomerService extends Service<TypeCustomer> {
-
-}
--- Module2_Web(2)/demo2/src/main/java/com/codegym/demo2/validation/CustomerValidation.java
+++
@@ -1,4 +0,0 @@
-package com.codegym.demo2.validation;
-
-public class CustomerValidation {
-}
